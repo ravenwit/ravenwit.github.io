6633f879fdbcb0fdea8ddb4a260a833f8ba8cf72
ravenwit <s.ahmed7733@gmail.com>
2023-06-09 14:28:38 +0200

    expedition checkbox fix

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9f748bc3cfb2b13aaf04.js"
+    "url": "webpack-runtime-068f8d5767e9d87f0a2d.js"
   },
   {
     "url": "framework-7958dc1d8ef573bc9962.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d19ee540f8127d390b203a6c24da7baa"
+    "revision": "ed015e5e9edab4b243cffc0eda89c056"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8c31644dfb0462cf0efb.js"
